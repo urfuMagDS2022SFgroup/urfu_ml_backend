d046b7ae1db705dd56f3345b8b43b8d401fb1d0d
Anton Bessolitsyn <anton.bessolitsyn@opencascade.com>
2022-11-18 23:55:20 +0300

    add sample_model file

---
+++ sample_model.py
@@ -0,0 +1,16 @@
+from transformers import pipeline
+class sample_model:
+    input_text: str
+    predicted_sentiment_score: int
+    predicted_sentiment_label: str
+
+    def __init__(self, sentence):    
+        self.input_text = sentence    
+        self.get_classifier()
+   
+    def get_classifier(self):
+        cls = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
+        predicted_sentiment = cls(self.input_text)
+        self.predicted_sentiment_score = round(predicted_sentiment[0]['score'], 3)
+        self.predicted_sentiment_label = predicted_sentiment[0]['label']
+
--- streamlit_demo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("HW")
-st.write("""
-Hello, World!
-""")
